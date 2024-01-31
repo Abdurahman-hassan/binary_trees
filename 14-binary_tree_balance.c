aa2456116ff6ed2ccc579a276be0cf6b8626dd8e
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-01-31 03:06:54 +0200

    Fix linker issue, add main file, add comments

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- *
- * @tree: Pointer to the root in tree or subtree
- *
- * Return: Balance factor
-*/
-
+ * @tree: Pointer to the root of the tree or subtree
+ * This function recursively traverses the tree,
+ * subtracting the height of the
+ * right subtree from the height of the left
+ * subtree at each node to calculate the balance factor.
+ * Return: Balance factor of the tree or subtree
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int node_balance = 0;
+	int left_height = 0, right_height = 0;
 
+	/* If tree is NULL, return 0 */
 	if (!tree)
 		return (0);
-	node_balance = binary_tree_height(tree->left);
-	node_balance -= binary_tree_height(tree->right);
-	return (node_balance);
+
+	/* Recursively measure the height of the left subtree */
+	if (tree->left)
+		left_height = 1 + binary_tree_balance(tree->left);
+
+	/* Recursively measure the height of the right subtree */
+	if (tree->right)
+		right_height = 1 + binary_tree_balance(tree->right);
+
+	/* Return the difference between the heights of the left and right subtrees */
+	return (left_height - right_height);
 }
