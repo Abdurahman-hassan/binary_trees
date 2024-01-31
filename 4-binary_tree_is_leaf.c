067a9ef4dd6a3548cccc6eecd5ab7fbddd3dcd19
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-01-31 02:09:25 +0200

    Add comments in 4-binary_tree_is_leaf.c add main file in order to test with

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_is_leaf - Checking whether the node is leaf or not
- *
  * @node: Pointer to node
- *
  * Return: 1 if node is a leaf, otherwise 0
 */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	/* check if node is NULL */
+	/* check if node has left or right child */
+	/* if node is NULL or has left or right child, return 0 */
+	/* otherwise, return 1 */
 	if (node == NULL || node->left || node->right)
 		return (0);
 	return (1);
