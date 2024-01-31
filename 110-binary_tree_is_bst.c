af69f93c5722d7364f0ebe4012b38a4d0c15e67f
nono <nour98nour@gmail.com>
2024-01-31 03:20:54 +0200

    fixed task 24 added tree_size function

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -3,6 +3,34 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - measure the size of a binary tree
+ * @tree: Pointer to the root of the tree or subtree
+ * Return: Size of the tree or subtree
+ */
+
+size_t binary_tree_size2(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	/* if tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
+
+	/* if tree has no children, return 1 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* traverse left subtree */
+	left_size = binary_tree_size(tree->left);
+	/* traverse right subtree */
+	right_size = binary_tree_size(tree->right);
+
+	/* return the sum of the sizes of the subtrees */
+	return (left_size + right_size + 1);
+}
+
+/**
  * binary_tree_inorder - goes through a binary tree using in-order traversal
  *
  * @tree: pointer to the root node of the tree to traverse
@@ -35,7 +63,7 @@ void binary_tree_inorder2(const binary_tree_t *tree, int *arr, size_t *idx)
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	size_t tree_sz = binary_tree_size(tree), i = 0;
+	size_t tree_sz = binary_tree_size2(tree), i = 0;
 	int *arr;
 
 	if (tree == NULL)
