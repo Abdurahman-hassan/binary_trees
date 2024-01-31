70372509fd3a2dccd43ffd921cd320e7d6f982b2
nono <nour98nour@gmail.com>
2024-02-01 01:34:39 +0200

    finished task 36

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -38,23 +38,33 @@ int is_complete(const binary_tree_t *tree, int index, int nodes)
 	return (is_complete(tree->left, 2 * index + 1, nodes) &&
 			is_complete(tree->right, 2 * index + 2, nodes));
 }
-
 /**
- * binary_tree_is_complete - checks if a binary tree is complete
+ * root_is_max - checks if a root is max in tree and recursively for all nodes
  * @tree: pointer to the root node of the tree to check
- * Return: 1 if tree is complete, 0 otherwise
+ * Return: 1 if root is max, 0 otherwise
  */
-int binary_tree_is_complete(const binary_tree_t *tree)
+int root_is_max(const binary_tree_t *tree)
 {
-	int nodes, index = 0;
-
+	if (tree == NULL)
+		return (1);
+	if (tree->left && tree->n <= tree->left->n)
+		return (0);
+	if (tree->right && tree->n <= tree->right->n)
+		return (0);
+	return (root_is_max(tree->left) && root_is_max(tree->right));
+}
+/**
+ * binary_tree_is_heap - checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is heap, 0 otherwise
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	int complete_flag = 0;
 	/* if tree is NULL, it is not complete */
 	if (!tree)
 		return (0);
-
-	/* get the number of nodes in the tree */
-	nodes = binary_tree_size(tree);
-
-	/* check if the tree is complete */
-	return (is_complete(tree, index, nodes));
+	
+	complete_flag = is_complete(tree, 0, binary_tree_size(tree));
+	return (complete_flag && root_is_max(tree));
 }
