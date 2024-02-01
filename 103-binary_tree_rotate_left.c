9aa62fcb856b34d233401cab7310603aba27d754
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-01 03:57:51 +0200

    update the rotate left function

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,30 +7,32 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	/* If tree is NULL or has no right child, return tree */
 	if (tree == NULL || tree->right == NULL)
 		return (tree);
 
-	/* Perform the rotation */
 	binary_tree_t *new_root = tree->right;
-
 	tree->right = new_root->left;
 
 	if (new_root->left != NULL)
+	{
 		new_root->left->parent = tree;
+	}
 
 	new_root->left = tree;
 	binary_tree_t *parent = tree->parent;
-
 	tree->parent = new_root;
 	new_root->parent = parent;
 
 	if (parent != NULL)
 	{
 		if (parent->left == tree)
+		{
 			parent->left = new_root;
+		}
 		else
+		{
 			parent->right = new_root;
+		}
 	}
 	return (new_root);
 }
