fb5f2d6d7a4bca3694d35884543f0c3f88597678
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-01 04:28:51 +0200

    Add docstring

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -2,7 +2,8 @@
 
 /**
  * array_to_avl - Builds an AVL tree from an array
- * @tree: Pointer to the first element of the array to be converted
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
  * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *array_to_avl(int *array, size_t size)
