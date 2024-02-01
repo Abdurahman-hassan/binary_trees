971a93f262d450610398b6951aa55669080134dc
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-01 05:43:12 +0200

    fix logic

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,43 +1,46 @@
 #include "binary_trees.h"
 
 /**
+ * avl_search - Searches for a value in an AVL tree
+ * @tree: Pointer to the root node of the AVL tree
+ * @value: The value to search for
+ *
+ * Return: 1 if value exists in the tree, 0 otherwise
+ */
+int avl_search(avl_t *tree, int value)
+{
+	if (!tree)
+		return (0);
+
+	if (value < tree->n)
+		return (avl_search(tree->left, value));
+	else if (value > tree->n)
+		return (avl_search(tree->right, value));
+	else
+		return (1);
+}
+
+/**
  * array_to_avl - Builds an AVL tree from an array
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
+ *
  * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	avl_t *root = NULL;
+	avl_t *tree = NULL;
+	size_t i;
 
-	size_t i = 0;
-	size_t j = 0;
-	size_t last = 0;
-
-	int arr[size];
-
-	if (array == NULL || size == 0)
+	if (!array)
 		return (NULL);
 
-	for (i = 0; i < size; ++i)
+	for (i = 0; i < size; i++)
 	{
-		arr[i] = array[i];
+		/* Insert only if the value is not already present in the tree */
+		if (!avl_search(tree, array[i]))
+			avl_insert(&tree, array[i]);
 	}
 
-	for (i = 0; i < size; ++i)
-	{
-		int flag = 1;
-
-		for (j = 0; j < i; ++j)
-		{
-			if (arr[i] == arr[j])
-				flag = 0;
-		}
-		if (flag)
-			arr[last++] = arr[i];
-	}
-	for (i = 0; i < last; ++i)
-		if (avl_insert(&root, arr[i]) == NULL)
-			return (NULL);
-	return (root);
+	return (tree);
 }
