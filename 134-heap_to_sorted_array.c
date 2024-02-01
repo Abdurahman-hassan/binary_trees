bd2d62179d4e88da5954d0285fbe9fa90c3b10f1
nono <nour98nour@gmail.com>
2024-02-01 21:36:53 +0200

    hot fix 40 task

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -6,13 +6,13 @@
  * Return: size of the tree, or 0 if tree is NULL
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_size2(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	/* recursively call the function */
 	/* to get the size of the left and right subtrees */
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	return (binary_tree_size2(tree->left) + 1 + binary_tree_size2(tree->right));
 }
 
 /**
@@ -26,12 +26,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int *arr = malloc(sizeof(int) * binary_tree_size(heap));
+	int *arr = malloc(sizeof(int) * binary_tree_size2(heap));
 	size_t i = 0;
 
 	if (arr == NULL || heap == NULL || size == NULL)
 		return (NULL);
-	*size = binary_tree_size(heap);
+	*size = binary_tree_size2(heap);
 	for (i = 0; i < *size; ++i)
 		arr[i] = heap_extract(&heap);
 	return (arr);
